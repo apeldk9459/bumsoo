4da524fd5ae7e6bff7830935ce85e3ed4e5fc011
Park <apeldk9459@nate.com>
2018-11-13 16:31:51 +0900

    second

---
+++ app/src/main/java/com/alarmy/konyang/alarmy/Constant.java
@@ -0,0 +1,6 @@
+package com.alarmy.konyang.alarmy;
+
+public class Constant {
+    public static String LOGIN_URL="http://192.168.0.6:3000/users/login/";
+    public static String Signup_URL="http://192.168.0.6:3000/users/signup/";
+}
